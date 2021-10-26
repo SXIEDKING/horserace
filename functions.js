0bb887b896c919c877320fb86fe74eb9721f3ae4
SaiedIslam <saiedislam61@gmail.com>
2021-10-26 10:12:56 +0100

    words

---
+++ functions.js
@@ -0,0 +1,13 @@
+/*function sayHello(){
+    alert("What ends you from ${name}, you're an opp!");
+}
+
+sayHello()
+
+let n = Saied */
+
+ let words=["Fuck","Cunt","Twat","Slag","Wanker"]
+
+ Array.splice(index,5,item)
+
+ console.log(words.replaceAll(`Fuck`,`****`))
