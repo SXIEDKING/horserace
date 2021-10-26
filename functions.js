cb083445455217613a00cbc813eb198c885a7605
SaiedIslam <saiedislam61@gmail.com>
2021-10-26 10:28:41 +0100

    result

--- functions.js
+++ functions.js
@@ -8,4 +8,16 @@ let n = Saied */
 
  let words=["Fuck","Cunt","Twat","Slag","Wanker"]
 
- console.log(words.replaceAll(`Fuck`,`****`))
+ console.log(words())
+
+ const search = `Fuck`
+ const search = `Cunt`
+ const search = `Twat`
+ const search = `Slag`
+ const search = `Wanker`
+
+ const replaceWith = `*****`
+
+ const result = replaceAll(search, replaceWith)
+
+ console.log(result)
